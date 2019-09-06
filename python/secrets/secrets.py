718934ff961196f5927d066809eb03aae8f62aef
excal04 <jeffrey@horangi.com>
2019-09-06 10:45:04 +0700

    fake changes

--- python/secrets/secrets.py
+++ python/secrets/secrets.py
@@ -5,3 +5,5 @@ PASSWORD = ".Td7nUkk9:09?78x49Jb.1(B|0$c$5ML!sc0ï¿½{y{kB1870;9+G"
 FACEBOOK_TOKEN = "EAACEdEose0cBA123asda"
 
 TWITTER_ACCESS_TOKEN = "101521180-wi5CJ3qLBikCSPN0WZJl8g7sMhYy7hCRkvkAwqGP"
+
+# some test changes to test something something
